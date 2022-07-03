649d23a3c53aa11f1d6ba65c82c2f6856d793bc6
FrenchWW <filebrute@gmail.com>
2022-07-03 23:08:57 +0100

    fixed login form

--- login.php
+++ login.php
@@ -131,7 +131,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         </div>
         <!-- Spinner End -->
 
-
         <!-- Sign In Start -->
         <div class="container-fluid">
             <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
@@ -143,13 +142,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             </a>
                             <h3>Sign In</h3>
                         </div>
+                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                            <label for="floatingInput">Email address</label>
+                            <label for="username">Email address</label>
+                            <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
                         </div>
                         <div class="form-floating mb-4">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                            <label for="floatingPassword">Password</label>
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
                         </div>
                         <div class="d-flex align-items-center justify-content-between mb-4">
                             <div class="form-check">
@@ -159,7 +159,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             <a href="">Forgot Password</a>
                         </div>
                         <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign In</button>
-                        <p class="text-center mb-0">Don't have an Account? <a href="">Sign Up</a></p>
+                        <p class="text-center mb-0">Don't have an Account? <a href="register.php">Sign Up</a></p>
                     </div>
                 </div>
             </div>
