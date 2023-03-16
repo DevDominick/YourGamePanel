0aead17e98e688a14ebe535a583232cf8fe78813
FileBrute <88153990+FileBrute@users.noreply.github.com>
2023-03-16 10:49:02 +0000

    update to pdo + theme changes

--- app/admin/uploadtheme.php
+++ app/admin/uploadtheme.php
@@ -1,6 +1,42 @@
 <?php
-$target_directory = __DIR__ . "themes/";
-$target_file = $target_directory . basename($_FILES["theme_file"]["name"]);
-move_uploaded_file($_FILES["theme_file"]["tmp_name"], $target_file);
-echo "File uploaded successfully.";
+// Handle theme upload
+if(isset($_POST['uploadTheme'])) {
+    // Validate file type
+    $allowedTypes = array('zip'); // Allow only zip files
+    $fileType = pathinfo($_FILES['theme']['name'], PATHINFO_EXTENSION);
+    if(!in_array($fileType, $allowedTypes)) {
+      echo "Invalid file type. Please select a zip file.";
+      exit;
+    }
+  
+    // Validate file size
+    $maxSize = 1000000; // Maximum file size in bytes
+    if($_FILES['theme']['size'] > $maxSize) {
+      echo "File size exceeds limit. Please select a smaller file.";
+      exit;
+    }
+  
+    // Move file to custom theme directory
+    $targetDir = "themes/";
+    $targetFile = $targetDir . basename($_FILES['theme']['name']);
+    if(move_uploaded_file($_FILES['theme']['tmp_name'], $targetFile)) {
+      // Update database with theme information
+      $themeName = $_POST['themeName'];
+      $themeVersion = $_POST['themeVersion'];
+      $themeFile = $targetFile;
+      // Use PDO to insert data into the database
+      $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+      $stmt = $pdo->prepare("INSERT INTO themes (name, version, file) VALUES (:name, :version, :file)");
+      $stmt->bindParam(':name', $themeName);
+      $stmt->bindParam(':version', $themeVersion);
+      $stmt->bindParam(':file', $themeFile);
+      $stmt->execute();
+      // Success message
+      echo "Theme uploaded successfully!";
+    } else {
+      // Error message
+      echo "Failed to upload theme. Please try again.";
+    }
+  }
+  
 ?>
--- server/config.php
+++ server/config.php
@@ -7,10 +7,11 @@ define('DB_PASSWORD', '');
 define('DB_NAME', '');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+try {
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("ERROR: Could not connect. " . $e->getMessage());
 }
 ?>
