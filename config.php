7315f3d3c608df82f563cc90176e23f105fcaf88
Dominick <88153990+DevDominick@users.noreply.github.com>
2023-03-18 02:38:44 +0000

    added config

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+// Configuration options
+#####################################
+#                                   #
+#          YourGamePanel            # 
+#                                   #
+#####################################
+
+$siteName = "YourGamePanel";
+$domain = "localhost";
+
+// Payment Options
+$sellixApiKey = "your_sellix_api_key_here";
+$sellixWebhookSecret = "your_sellix_webhook_secret_here";
+
+$coinbaseCommerceApiKey = "your_coinbase_commerce_api_key_here";
+$coinbaseCommerceWebhookSecret = "your_coinbase_commerce_webhook_secret_here";
+?>
