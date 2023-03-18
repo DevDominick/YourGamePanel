c2660ed2170a1244b364150c669ad246cc439b93
Dominick <88153990+DevDominick@users.noreply.github.com>
2023-03-18 01:37:42 +0000

    Update index.php

--- index.php
+++ index.php
@@ -129,9 +129,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
                 <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
                     <div class="bg-secondary rounded p-4 p-sm-5 my-4 mx-3">
-                        <div class="d-flex align-items-center justify-content-between mb-3">
+                        <div class="d-flex align-items-center justify-content-between mb-1">
                             <a href="index.html" class="">
-                                <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>YourGamePanel</h3>
+                                <h3 class="text-primary"><i class="fa fa-user-edit me-1"></i>YourGamePanel</h3>
                             </a>
                             <h3>Sign In</h3>
                         </div>
